6a66330f68b822929f46594993777d674ecfa5b1
Andy Chen <acchen@cpp.edu>
2022-06-04 15:37:35 -0700

    initialized bot

--- scr/index.js
+++ scr/index.js
@@ -7,6 +7,10 @@ const { Client, Intents } = require('discord.js');
 // instantiate new client instance
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
+// run when client is ready
 client.once('ready', () => {
    console.log('Ready!');
 });
+
+// login with client token
+client.login(process.env.TOKEN);
