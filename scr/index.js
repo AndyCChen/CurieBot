fb8ebaca13abb16b1c372a68d7787c05e9b367c1
Andy Chen <acchen@cpp.edu>
2022-06-05 18:08:08 -0700

    implement slash commands

--- scr/index.js
+++ scr/index.js
@@ -1,8 +1,10 @@
 // load token from .env file
 require('dotenv').config({ path: '../.env'});
 
-// Import discord.js
-const { Client, Intents } = require('discord.js');
+// Imports
+const { Client, Intents, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 // instantiate new client instance
 const client = new Client(
@@ -13,9 +15,41 @@ const client = new Client(
    }
 );
 
+client.commands = new Collection();
+
+// path to commands folder containing all commands
+const commandsPath = path.join(__dirname, 'commands');
+
+// array of all filenames inside of commands folder
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+   const filePath = path.join(commandsPath, file);
+   const command = require(filePath);
+
+   // set new command to collection with key as name and value as the exported module
+   client.commands.set(command.data.name, command);
+}
+
 // run when client is ready
 client.once('ready', () => {
    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+   // return if interation is not a command
+   if (!interaction.isCommand()) return;
+
+   const command = client.commands.get(interaction.commandName);
+
+   if (!command) return;
+
+   try {
+      await command.execute(interaction);
+   } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+   }
 });
 
 // login with client token
