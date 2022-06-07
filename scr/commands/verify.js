28bf27056a0c6efd4a383c10d3da8109e8f2f977
Andy Chen <acchen@cpp.edu>
2022-06-06 22:50:55 -0700

    save steam id into firestore database

--- scr/commands/verify.js
+++ scr/commands/verify.js
@@ -1,26 +1,52 @@
+// check if steam id is valid based on if a player is returned from GetPlayerSummaries
+
 require('dotenv').config({ path: '../../.env'});
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
 module.exports = {
    data: new SlashCommandBuilder()
       .setName('verify')
-      .setDescription('verify steam id.'),
+      .setDescription('verify steam id.')
+      .addStringOption(option =>
+         option.setName('steam_id')
+            .setDescription('SteamID argument')
+            .setRequired(true)),
    async execute(interaction) {
+
+      // escape special markdown characters in discord chat
+      function escapeMarkdown(text) {
+         return text.replace(/(\*|_|`|\\)/g, '\\$1');
+      }
+
       const key = process.env.KEY
-      const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${key}&steamids=76561198241832058`
+      const steam_ID = interaction.options.getString('steam_id');
+      const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${key}&steamids=${steam_ID}`;
 
       const response = await fetch(url);
       const data = await response.json();
 
       const playerCount = data.response.players.length;
-
-      console.log(playerCount);
+      const playerName = escapeMarkdown(data.response.players[0].personaname);
+      const discordID = interaction.user.id;
+      console.log(interaction.user.id);
 
       if (playerCount != 1) {
          await interaction.reply('Error, steam id not associated with a account.');
       } else {
-         await interaction.reply('Success!');
+         // instantiate firestore
+         const db = getFirestore();
+
+         // reference to user collection and discordID document
+         const docRef = db.collection('users').doc(discordID);
+
+         // write steam_ID into database
+         await docRef.set({
+            steamID: steam_ID,
+         });
+
+         await interaction.reply(`Success!\nHeyo ${playerName}`);
       }
    }
 }
